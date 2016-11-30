caec549320062ae7e0d29f90aaffd052fb74f4f0
Kristian Snyder <knsnyder96@gmail.com>
2016-11-30 17:51:49 -0500

    Added TLS configuration Cleaned up page pre-login

--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/snyderks/spotkov-web/handlers"
+	"github.com/snyderks/spotkov/configRead"
 )
 
 func main() {
+	config, err := configRead.ReadConfig("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		http.Redirect(w, req,
+			"https://"+config.Hostname+config.TLSPort+req.URL.String(),
+			http.StatusMovedPermanently)
+	})
+	go http.ListenAndServe(config.HTTPPort, mux)
+
 	handlers.SetUpAPICalls()
 	handlers.SetUpBasicHandlers()
 	fmt.Println("Successfully initialized!")
-	http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServeTLS(config.TLSPort, "cert.pem", "key.pem", nil)
+	log.Fatal(err)
 }
